0fcf33cb8eb64e99a576da5201b84193b68f58c4
Mariana Terintii <117921869+marianaterintii@users.noreply.github.com>
2022-11-19 20:59:51 +0200

    Add files via upload

---
+++ 2d.for.pattern.py
@@ -0,0 +1,51 @@
+# PATTERN: 10*10
+
+# * * * * * * * * * * 
+# * * * * * * * * * * 
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+# * * * * * * * * * *
+from os import system
+
+system ("cls")
+
+rx = 5
+ry = 5
+print ()
+
+for y in range(1,11):
+
+    for x in range (1,11):
+        if x == rx and y == ry:
+            print ("R", end= " ")
+
+        elif x == rx and y == ry - 1:
+            print ("+", end= " ")
+        elif x == rx and y == ry + 1:
+            print ("+", end= " ")
+
+        elif x == rx - 1 and y == ry :
+            print ("+", end= " ")
+        elif x == rx + 1 and y == ry:
+            print ("+", end= " ") 
+
+        elif x == 4 and y == 4:
+            print ("+", end= " ")
+        elif x == 6 and y == 4:
+            print ("+", end= " ")
+
+        elif x == 4 and y == 6:
+            print ("+", end= " ")
+        elif x == 6 and y == 6:
+            print ("+", end= " ")
+
+        else:
+            print (".", end=" ")
+    print ()
+
+print ()
